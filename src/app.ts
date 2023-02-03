19d39bbc861b9d25336bfadf21d30632b1756486
Joakim Ottosson <joakim.ottosson@medieinstitutet.se>
2023-02-03 10:00:52 +0100

    some minor fixes and added morgan to dev. And also did a build. Not included to git

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import routes from './routes'
+import morgan from "morgan";
 
 // Init app
 const app = express()
 
 // Middleware
 app.use(express.json())
+app.use(morgan('dev'))
 
 // Routes:
 app.use(routes)
